6b3559fd2e63c88c5dd061482d21f02db598d018
yusuf0786 <yusssuf0786@gmail.com>
2021-01-06 14:54:50 +0530

    five commit

--- js/script.js
+++ js/script.js
@@ -2,26 +2,27 @@ $(function(){
 	
 	$(".banner h1").fadeIn(1500);
 	$(".banner p").fadeIn(1500);
-	$(".my-button").show(0580);
-	
-	
+	$(".my-button").animate({
+		marginLeft: 50,
+		opacity: 1
+	},0580);
 });
 
 $(document).scroll(function(){
-    //Show element after user scrolls 1000px
-    var y = $(this).scrollTop();
-    if (y > 1000) {
-        $('.images-section .section, .images-section button').slideDown();
-    } else {
-        $('.images-section .section, .images-section button').slideUp();
-    }
+	//Show element after user scrolls 1000px
+	var y = $(this).scrollTop();
+	if (y > 1000) {
+		$('.images-section .section, .images-section .text-center').slideDown();
+	} else {
+		$('.images-section .section, .images-section .text-center').slideUp();
+	}
 	
-	$('.images-section .section .right-section h3, .images-section .section .right-section p').each(function () {
-        var t = $(this).parent().offset().top + -158;
-        if (y > t) {
-            $(this)slideDown(1000);
-        } else {
+	$('.images-section .section .right-section h3, .images-section .section .right-section p, .images-section .section .right-section button').each(function () {
+		var t = $(this).parent().offset().top + -158;
+		if (y > t) {
+			$(this).slideDown(1000);
+		} else {
             $(this).slideUp(1000);
-        }
-    });
+		}
+	});
 });
